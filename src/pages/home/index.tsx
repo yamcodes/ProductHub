e31ca71eaf9df0dd756b7979a04ce89b4a90c7fa
yamyam263 <yamyam263@gmail.com>
2023-10-24 22:04:38 +0300

    feat: remove template code

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,31 +1,9 @@
-import { useState } from 'react';
-import reactLogo from '/react.svg';
-import viteLogo from '/vite.svg';
-import ProductsList from '~/widgets/products-list';
 import { AddProductForm } from '~/features/add-product';
+import ProductsList from '~/widgets/products-list';
 
 export default function Home() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <ProductsList />
       <AddProductForm />
     </>
