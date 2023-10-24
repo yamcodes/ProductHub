2131c3df2739abcc27e091d7260400b25aea742e
yamyam263 <yamyam263@gmail.com>
2023-10-24 22:35:01 +0300

    feat: tanstack form

--- src/features/add-product/ui/index.tsx
+++ src/features/add-product/ui/index.tsx
@@ -1,32 +1,67 @@
 import { useAddProduct } from '..';
-import { useForm, SubmitHandler } from 'react-hook-form';
-
-interface FormValues {
-  name?: string;
-  quantity?: number;
-  brand?: string;
-}
+import { useForm } from '@tanstack/react-form';
 
 export function Form() {
   const { mutate } = useAddProduct();
-  const { register, handleSubmit } = useForm<FormValues>();
-
-  const onSubmit: SubmitHandler<FormValues> = (data) => {
-    const { name, quantity, brand } = data;
-
-    if (!name || !quantity || !brand) {
-      console.log('Invalid input');
-      return;
-    }
-    mutate({ name, quantity, brand });
-  };
+  const form = useForm({
+    // memoize the default values to prevent re-renders
+    defaultValues: {
+      name: '',
+      quantity: 0,
+      brand: '',
+    },
+    onSubmit: (values) => mutate(values),
+  });
+  const { Provider, Field } = form;
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register('name')} placeholder="name" />
-      <input {...register('quantity')} placeholder="quantity" type="number" />
-      <input {...register('brand')} placeholder="brand" />
-      <button type="submit">Submit</button>
-    </form>
+    <Provider>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+      >
+        <Field
+          name="name"
+          children={(field) => (
+            <input
+              name={field.name}
+              value={field.state.value}
+              onChange={(e) => field.handleChange(e.target.value)}
+              onBlur={field.handleBlur}
+              placeholder="name"
+            />
+          )}
+        />
+        <Field
+          name="quantity"
+          children={(field) => (
+            <input
+              name={field.name}
+              value={field.state.value}
+              onChange={(e) => field.handleChange(e.target.valueAsNumber)}
+              onBlur={field.handleBlur}
+              type="number"
+              placeholder="quantity"
+            />
+          )}
+        />
+        <Field
+          name="brand"
+          children={(field) => (
+            <input
+              name={field.name}
+              value={field.state.value}
+              onChange={(e) => field.handleChange(e.target.value)}
+              onBlur={field.handleBlur}
+              placeholder="brand"
+            />
+          )}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </Provider>
   );
 }
