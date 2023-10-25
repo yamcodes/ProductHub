a5de80690b1702cbebcb682cee506e40c823ab5f
yamyam263 <yamyam263@gmail.com>
2023-10-25 04:33:07 +0300

    feat: zod validations

--- src/features/add-product/ui/index.tsx
+++ src/features/add-product/ui/index.tsx
@@ -1,67 +1,62 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 import { useAddProduct } from '..';
-import { useForm } from '@tanstack/react-form';
+
+interface FormValues {
+  name: string;
+  quantity: number;
+  brand: string;
+}
+
+const schema = z.object({
+  name: z.string().min(3, 'Name must be at least 3 characters long'),
+  quantity: z
+    .number()
+    .min(1, 'Quantity must be between 1 and 10')
+    .max(10, 'Quantity must be between 1 and 10'),
+  brand: z.string().min(1, 'Brand must not be empty'),
+});
 
 export function Form() {
   const { mutate } = useAddProduct();
-  const form = useForm({
-    // memoize the default values to prevent re-renders
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isDirty },
+  } = useForm<FormValues>({
+    resolver: zodResolver(schema),
     defaultValues: {
       name: '',
-      quantity: 0,
+      quantity: 1,
       brand: '',
     },
-    onSubmit: (values) => mutate(values),
   });
-  const { Provider, Field } = form;
 
   return (
-    <Provider>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          form.handleSubmit();
-        }}
-      >
-        <Field
-          name="name"
-          children={(field) => (
-            <input
-              name={field.name}
-              value={field.state.value}
-              onChange={(e) => field.handleChange(e.target.value)}
-              onBlur={field.handleBlur}
-              placeholder="name"
-            />
-          )}
+    <form onSubmit={handleSubmit((values) => mutate(values))}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input {...register('name')} placeholder="Bottle (Glass)" type="text" />
+        {errors.name?.message && <div>{errors.name?.message}</div>}
+      </div>
+      <div>
+        <label htmlFor="quantity">Quantity</label>
+        <input
+          {...register('quantity', { valueAsNumber: true })}
+          placeholder="1"
+          type="number"
         />
-        <Field
-          name="quantity"
-          children={(field) => (
-            <input
-              name={field.name}
-              value={field.state.value}
-              onChange={(e) => field.handleChange(e.target.valueAsNumber)}
-              onBlur={field.handleBlur}
-              type="number"
-              placeholder="quantity"
-            />
-          )}
-        />
-        <Field
-          name="brand"
-          children={(field) => (
-            <input
-              name={field.name}
-              value={field.state.value}
-              onChange={(e) => field.handleChange(e.target.value)}
-              onBlur={field.handleBlur}
-              placeholder="brand"
-            />
-          )}
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </Provider>
+        {errors.quantity?.message && <div>{errors.quantity?.message}</div>}
+      </div>
+      <div>
+        <label htmlFor="brand">Brand</label>
+        <input {...register('brand')} placeholder="Coca Cola" />
+        {errors.brand?.message && <div>{errors.brand?.message}</div>}
+      </div>
+      <button type="submit" disabled={!isDirty}>
+        Submit
+      </button>
+    </form>
   );
 }
