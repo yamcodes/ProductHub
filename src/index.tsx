d74beaa9243cd7d2532578ce5094ee029143fbea
yamyam263 <yamyam263@gmail.com>
2023-10-26 23:52:41 +0300

    feat: style reset

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './app/index.tsx';
 import './index.css';
 import 'virtual:uno.css';
+import '@unocss/reset/tailwind-compat.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
