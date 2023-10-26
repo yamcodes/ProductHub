69ee4756d73f1fee28f79c7bb9f2153ad1274c29
yamyam263 <yamyam263@gmail.com>
2023-10-26 23:35:49 +0300

    feat: headless ui preset

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,11 @@
 // uno.config.ts
 import { defineConfig, presetUno, presetIcons } from 'unocss';
+import { presetHeadlessUi } from 'unocss-preset-primitives';
 
 export default defineConfig({
   presets: [
     presetUno(),
+    presetHeadlessUi(),
     presetIcons({
       extraProperties: {
         display: 'inline-block',
