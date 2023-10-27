333b652b25467f2f2ffa901f019cdd459389efe8
yamyam263 <yamyam263@gmail.com>
2023-10-27 16:49:31 +0300

    feat: red delete button

--- src/features/delete-product/ui.tsx
+++ src/features/delete-product/ui.tsx
@@ -9,7 +9,10 @@ interface Props {
 export function Button({ productId }: Props) {
   const { mutate } = useDeleteProduct();
   return (
-    <UiButton onClick={() => mutate(productId)} className="bg-red">
+    <UiButton
+      onClick={() => mutate(productId)}
+      className="bg-red-5 text-white hover:bg-red-6 border-0"
+    >
       <div className="i-tabler:trash mr-1 mb-2px" />
       Delete
     </UiButton>
