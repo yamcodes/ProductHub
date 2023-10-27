be0aaf8b692c70c8ffebeed04ab8e47ee1f57de9
yamyam263 <yamyam263@gmail.com>
2023-10-27 15:39:50 +0300

    feat: textbox styles

--- src/shared/ui/Textbox.tsx
+++ src/shared/ui/Textbox.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps, forwardRef } from 'react';
+import clsx from 'clsx';
 
 interface Props extends ComponentProps<'input'> {
   label?: string;
@@ -8,16 +9,27 @@ interface Props extends ComponentProps<'input'> {
 const Textbox = forwardRef<HTMLInputElement, Props>(
   ({ className, name, errorMessage, ...rest }, ref) => {
     return (
-      <div>
-        <label htmlFor={name}>{rest.label}</label>{' '}
+      <div className="flex flex-col">
+        <label htmlFor={name} className="text-sm font-medium mb-2">
+          {rest.label}
+        </label>
         <input
-          className={`bg-transparent py-1 px-2 rounded select-none cursor-pointer ${className}`}
+          className={clsx(
+            'bg-white text-sm border-#ddd border-1 shadow-sm py-1.5 px-3 rounded md:max-w-[300px] sm:max-w-screen',
+            errorMessage
+              ? 'border-red-400 text-red-700 focus:outline-red-500 placeholder-red-300'
+              : 'border-#ddd',
+            ' ',
+            className,
+          )}
           {...rest}
           name={name}
           id={name}
           ref={ref}
         />
-        {errorMessage && <div>{errorMessage}</div>}
+        {errorMessage && (
+          <div className="text-xs text-red-600 mt-1">{errorMessage}</div>
+        )}
       </div>
     );
   },
