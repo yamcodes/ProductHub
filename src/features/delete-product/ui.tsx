8eb11dd5ab9a3e215af52144979be1b268ba01b1
yamyam263 <yamyam263@gmail.com>
2023-10-27 18:52:21 +0300

    feat: disable delete button while delete

--- src/features/delete-product/ui.tsx
+++ src/features/delete-product/ui.tsx
@@ -1,17 +1,24 @@
 import { IProduct } from '~/entities/products';
 import { useDeleteProduct } from '.';
 import { Button as UiButton } from '~/shared/ui';
+import { twJoin } from 'tailwind-merge';
 
 interface Props {
   productId: IProduct['id'];
 }
 
 export function Button({ productId }: Props) {
-  const { mutate } = useDeleteProduct();
+  const { mutate, isPending: disabled } = useDeleteProduct();
   return (
     <UiButton
       onClick={() => mutate(productId)}
-      className="bg-red-5 text-white hover:bg-red-6 border-0"
+      className={twJoin(
+        'border-0',
+        disabled
+          ? 'bg-red-3 text-red-1'
+          : 'hover:bg-red-6 bg-red-5 text-white ',
+      )}
+      disabled={disabled}
     >
       <div className="i-tabler:trash mr-1 mb-2px" />
       Delete
