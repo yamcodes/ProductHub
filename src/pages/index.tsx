e471ef1f1f8d350a122c119ed320f19be9a04598
yamyam263 <yamyam263@gmail.com>
2023-10-27 03:02:39 +0300

    refactor: remove pages/root.tsx
    that was confusing as root is not a page but a type of a route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,50 @@
-import { RootRoute, Route, Router } from '@tanstack/react-router';
+import { Link, Outlet, RootRoute, Route, Router } from '@tanstack/react-router';
 import About from './about';
 import Home from './home';
-import { Root } from './root';
+import { lazy } from 'react';
+import { getTabIndex, tabs } from '~/shared/lib';
+import { Tab } from '~/shared/ui';
+
+// Define Devtools (only for development)
+/* eslint-disable react-refresh/only-export-components -- This is a dev-only component */
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      );
+
+function Root() {
+  return (
+    <div className="flex items-center flex-col">
+      <div className="w-full">
+        <div className="p-4">
+          <Link
+            to="/"
+            className="text-xl font-bold mx-6 bg-gradient-to-r from-pink-400 to-purple-500 bg-clip-text text-transparent cursor-pointer select-none
+        "
+          >
+            ProductHub
+          </Link>
+          <Tab.Group selectedIndex={getTabIndex(location.pathname)}>
+            {tabs.map((tab) => (
+              <Link to={tab.pathname} key={tab.pathname}>
+                <Tab>{tab.name}</Tab>
+              </Link>
+            ))}
+          </Tab.Group>
+        </div>
+      </div>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </div>
+  );
+}
 
 const rootRoute = new RootRoute({
   component: Root,
