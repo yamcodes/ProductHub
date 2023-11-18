e88fc73d869cf86a660672fa7374c8f5a3d8fdd4
Yam Borodetsky <hi@yam.codes>
2023-11-18 14:20:27 +0200

    refactor: move providers to providers/index

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { RouterProvider } from '@tanstack/react-router';
 import { router } from '~/routes';
-import { AppProvider } from './providers';
-import { withSuspense } from './providers/Suspense';
-import { withQueryClient } from './providers/QueryClientProvider';
-import { withIcons } from './providers/Icons';
+import { AppProvider, providers } from './providers';
 
 // Create the app by enhancing the router with HOCs
 export const App = () => (
-  <AppProvider providers={[withSuspense, withQueryClient, withIcons]}>
+  <AppProvider providers={providers}>
     <RouterProvider router={router} />
   </AppProvider>
 );
