e05f7a08f734f24bd337b3cfae06c865fb92fe84
Yam Borodetsky <hi@yam.codes>
2023-11-18 14:17:16 +0200

    refactor: expose app providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { RouterProvider } from '@tanstack/react-router';
 import { router } from '~/routes';
-import { withProviders } from './providers';
+import { AppProvider } from './providers';
+import { withSuspense } from './providers/Suspense';
+import { withQueryClient } from './providers/QueryClientProvider';
+import { withIcons } from './providers/Icons';
 
 // Create the app by enhancing the router with HOCs
-export const App = withProviders(() => <RouterProvider router={router} />);
+export const App = () => (
+  <AppProvider providers={[withSuspense, withQueryClient, withIcons]}>
+    <RouterProvider router={router} />
+  </AppProvider>
+);
--- src/providers/AppProvider/AppProvider.tsx
+++ src/providers/AppProvider/AppProvider.tsx
@@ -1,10 +1,15 @@
 import { FC, PropsWithChildren } from 'react';
 import { withProviders } from './withProviders';
+import { HOC } from '~/types';
+
+interface Props extends PropsWithChildren {
+  providers: HOC[];
+}
 
 /**
  * Provider for all providers in the app
  */
-export const AppProvider: FC<PropsWithChildren> = ({ children }) => {
-  const EnhancedApp = withProviders(() => <>{children}</>);
+export const AppProvider: FC<Props> = ({ providers, children }) => {
+  const EnhancedApp = withProviders(() => <>{children}</>, providers);
   return <EnhancedApp />;
 };
