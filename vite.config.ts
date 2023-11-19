555283cb9302fc8fe3c0c3916171001adc98eb55
Yam Borodetsky <hi@yam.codes>
2023-11-20 01:08:25 +0200

    feat: svgr and swc
    speed up development and alllow using svgs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import React from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [React()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [
       {
