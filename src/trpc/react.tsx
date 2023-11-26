758c2250251c88d6cdb7938583b14fe0406bff3e
Yam Borodetsky <hi@yam.codes>
2023-11-27 00:15:35 +0200

    fix: add vscode settings

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -1,12 +1,16 @@
-"use client";
+'use client';
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
-import { createTRPCReact } from "@trpc/react-query";
-import { useState } from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQueryClient,
+} from '@tanstack/react-query';
+import { loggerLink, unstable_httpBatchStreamLink } from '@trpc/client';
+import { createTRPCReact } from '@trpc/react-query';
+import { useState } from 'react';
 
-import { type AppRouter } from "@/server/api/root";
-import { getUrl, transformer } from "./shared";
+import { type AppRouter } from '@/server/api/root';
+import { getUrl, transformer } from './shared';
 
 export const api = createTRPCReact<AppRouter>();
 
@@ -15,27 +19,26 @@ export function TRPCReactProvider(props: {
   cookies: string;
 }) {
   const [queryClient] = useState(() => new QueryClient());
-
   const [trpcClient] = useState(() =>
     api.createClient({
       transformer,
       links: [
         loggerLink({
           enabled: (op) =>
-            process.env.NODE_ENV === "development" ||
-            (op.direction === "down" && op.result instanceof Error),
+            process.env.NODE_ENV === 'development' ||
+            (op.direction === 'down' && op.result instanceof Error),
         }),
         unstable_httpBatchStreamLink({
           url: getUrl(),
           headers() {
             return {
               cookie: props.cookies,
-              "x-trpc-source": "react",
+              'x-trpc-source': 'react',
             };
           },
         }),
       ],
-    })
+    }),
   );
 
   return (
