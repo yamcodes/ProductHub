ca9059bc17d1f0b479d4ff95dcfeed6ef4dd2d61
aruay99 <aruaiberdk@gmail.com>
2023-11-29 01:32:05 +0600

    fix: fix the table css

--- src/features/products/components/Product.tsx
+++ src/features/products/components/Product.tsx
@@ -12,17 +12,17 @@ export function Product({ product, pending, actions }: Props) {
   return (
     <>
       <table
-        className={twJoin(' w-full border-collapse', pending && 'opacity-50')}
+        className={twJoin('  mx-4 w-full border-collapse', pending && 'opacity-50')}
       >
         <tbody>
-          <tr className="border-b ">
-            <td className="p-4  text-left text-gray-500">
+          <tr className="border-b w-50x">
+            <td className="p-4  text-left text-gray-500 w-16">
               x{product.quantity}
             </td>
-            <td className="truncate break-all p-4 text-left ">
+            <td className="truncate break-all p-4 text-left w-64 ">
               {product.name}
             </td>
-            <td className="truncate break-all p-4 text-left">
+            <td className="truncate break-all p-4 text-left w-64">
               {product.brand}
             </td>
             <td>{actions}</td>
